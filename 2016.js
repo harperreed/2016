d7eafe24154d5355050b95415f3e7affab0680af
Harper Reed <harper@nata2.org>
2016-08-01 09:12:09 -0500

    initial commit

---
+++ 2016.js
@@ -0,0 +1,55 @@
+(function() {
+	function byId(id) {
+		return document.getElementById(id);
+	}
+
+	function formatTens(n) {
+		// format integers to have at least two digits
+		return (n < 10) ? '0'+n : ''+n;
+	}
+
+
+	// Mayan Calendar: 1356088271111
+
+
+	function update() {
+		var units =countdown.YEARS |
+    countdown.DAYS |
+    countdown.HOURS |
+    countdown.MINUTES |
+    countdown.SECONDS,
+			empty = "GO VOTE!" || null,
+			max = +(11),
+			digits = +(0);
+
+		
+		var yyyy = +(2016),
+			MM = +(11)-1,
+			dd = +(8),
+			HH = +(9),
+			mm = +(00),
+			ss = +(00),
+			fff = +(00);
+
+		var start = new Date(yyyy, MM, dd, HH, mm, ss, fff),
+			ts = countdown(start, null, units, max, digits);
+
+		var counter = byId('counter'),
+			msg = ts.toHTML('strong', empty);
+
+	
+		counter.innerHTML = msg;
+
+		// update timezone label
+		var tz = start.getTimezoneOffset();
+		if (tz) {
+			var tzh = Math.floor(Math.abs(tz) / 60),
+				tzm = (Math.abs(tz) % 60);
+
+		} else {
+		}
+
+		requestAnimationFrame(update);
+	}
+	update();
+})();
